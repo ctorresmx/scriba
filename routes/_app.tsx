7d0e6103fe26ee63f439ed55238a89f14744b0ba
Carlos Torres <j.carlos.torres@me.com>
2025-07-06 20:59:42 -0700

    Add env vars for blog config (#5)
    * Add env vars for blog config
    
    * Fix name/title mix up

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,11 +1,13 @@
 import { type PageProps } from "$fresh/server.ts";
+import { getBlogHeaderTitle } from "../utils/config.ts";
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Scriba</title>
+        <title>{getBlogHeaderTitle()}</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body class="font-mono">
---
+++ utils/config.ts
@@ -0,0 +1,17 @@
+import { BlogConfig } from "../types/blog.ts";
+
+export function getBlogHeaderTitle(pageHeader?: string): string {
+    const { title } = getBlogConfig();
+    if (pageHeader) {
+        return `${pageHeader} - ${title}`;
+    }
+    return `${title}`;
+}
+
+export function getBlogConfig(): BlogConfig {
+    return {
+        name: Deno.env.get("BLOG_NAME") || "Scriba",
+        title: Deno.env.get("BLOG_TITLE") || "Scriba",
+        copyright: Deno.env.get("BLOG_COPYRIGHT") || "Scriba",
+    };
+}
